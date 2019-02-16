091c032cf94853d2d0e72f5eb7aeae3c5142895a
Ahmed Badawy <ahmed.badawy4@vodafone.com>
2019-02-16 11:23:46 +0200

    add weather function

--- app.py
+++ app.py
@@ -1,26 +1,26 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from weather import query_api
+
+# remove this line in production
+#app.config['DEBUG'] = True
+
 
 app = Flask(__name__)
 
-weather = [
-        {
-          'city': 'Green Eggs and Ham',
-          'temp': 20,
-          '% chance of rain': '40%'
-        },
-]
+version = [ { 'Version': '0.0.1' }  ]
 
-version = [ { 'Weather API' : ' version: 0.0.1' }  ]
-
-#GET /weather
+#GET /weather?city=cairo
 @app.route('/weather')
 def get_weather():
-        return jsonify({'weather': weather})
+    city = request.args.get('city')
+    data = query_api(city)
+    return jsonify({'weather': data})
+
 
 #GET /version
 @app.route('/version')
 def get_version():
-        return jsonify({'version': version})
+        return jsonify({'weather API': version})
 
 #app.run(port=5000)
 app.run(host='0.0.0.0',port=5000)
