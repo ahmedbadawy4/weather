a852b0babfe647509a4da0bb9432f49463b1392e
Ahmed Badawy <ahmed.badawy4@vodafone.com>
2019-02-21 10:44:24 +0200

    edit token

--- weather.py
+++ weather.py
@@ -4,7 +4,7 @@ import pytz
 import requests
 import math
 import base64
-API_KEY = "fac345183f69a2b3f80148bbdd18db43""
+API_KEY = "fac345183f69a2b3f80148bbdd18db43"
 API_URL = ('http://api.openweathermap.org/data/2.5/weather?q={}&mode=json&units=metric&appid={}')
 
 #CITY = 'Cairo'
