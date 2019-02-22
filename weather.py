1761df3d0a6b1374c77696f9c88e4c3a6787b6d7
Ahmed Badawy <ahmed.badawy4@vodafone.com>
2019-02-22 03:12:46 +0200

    enhancements

--- weather.py
+++ weather.py
@@ -4,14 +4,17 @@ import pytz
 import requests
 import math
 import base64
+
 API_TOKEN = os.getenv("API_KEY")
 API_URL = ('http://api.openweathermap.org/data/2.5/weather?q={}&country()&mode=json&units={}&av_temp{}&av_wind{}&appid={}')
-av_temp = ""
-av_wind = ""
-def query_api(city, country, units, av_temp, av_wind,):
+temp_threshold = ""
+wind_threshold = ""
+
+
+def query_api(city, country, av_temp, av_wind, units):
     try:
-        print(API_URL.format(city, country, units, av_temp, av_wind, API_TOKEN))
-        data = requests.get(API_URL.format(city, units,av_temp , av_wind, API_TOKEN)).json()
+        print(API_URL.format(city, country, units, wind_threshold, wind_threshold, API_TOKEN))
+        data = requests.get(API_URL.format(city, units, temp_threshold, wind_threshold, API_TOKEN)).json()
     except Exception as exc:
         print(exc)
         data = None
